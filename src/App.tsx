be30c269fde9516f170bdc78048d8c6265f6aff9
Sahithyan <sahithyan2701@gmail.com>
2020-12-30 23:26:25 +0530

    first page half-done

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,24 @@ import { Typography, Button, Input, Form } from "antd";
 import { Result } from "./components/Result";
 import { ResultObj } from "./types";
 
-import "./style/main.scss";
 import "./style/theme.antd.less";
+import "./style/main.scss";
 
 const { Title } = Typography;
 
 export const App = () => {
-  const [result, setResult] = React.useState<ResultObj>(null);
+  const [result, setResult] = React.useState<any>(null);
 
   const onFinish = (values) => {
     console.log("Success:", values);
 
     fetch(`/api/main?password=${values["password"]}`)
-      .then((a) => a.json())
-      .then((c) => {
-        console.log(c);
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        setResult(result);
       })
-      .catch((w) => {
-        console.log(w);
-      });
+      .catch(console.error);
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -31,32 +30,36 @@ export const App = () => {
   };
 
   return (
-    <div className="app">
-      <Title className="main-title">Password Tester</Title>
-      <div>
-        {!result ? (
-          <Form
-            initialValues={{ password: "" }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-          >
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[{ required: true, message: "Give a password to test" }]}
+    <div className="home-page">
+      <div className="main-content">
+        <Title className="main-title">Password Tester</Title>
+        <div>
+          {!result ? (
+            <Form
+              layout="vertical"
+              initialValues={{ password: "" }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              requiredMark={false}
             >
-              <Input />
-            </Form.Item>
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: "Give a password to test" }]}
+              >
+                <Input />
+              </Form.Item>
 
-            <Form.Item>
-              <Button size="middle" type="primary" htmlType="submit">
-                Test
-              </Button>
-            </Form.Item>
-          </Form>
-        ) : (
-          <Result data={result} />
-        )}
+              <Form.Item>
+                <Button size="middle" type="primary" htmlType="submit">
+                  Test
+                </Button>
+              </Form.Item>
+            </Form>
+          ) : (
+            <Result data={result} />
+          )}
+        </div>
       </div>
 
       <div className="about">Created by Sahithyan with ❤️</div>
